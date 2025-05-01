d94aa639c2e6999895501dbb0d71f462926908ab
Adsonc321 <adson.silva@gruposapiens.com.br>
2025-05-01 14:28:37 -0300

    Add files via upload

---
+++ landing-clinica-growth/pages/index.js
@@ -0,0 +1,13 @@
+import Head from "next/head";
+import ClinicaLandingPage from "../components/ClinicaLandingPage";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Landing Clínica Growth</title>
+      </Head>
+      <ClinicaLandingPage />
+    </>
+  );
+}
